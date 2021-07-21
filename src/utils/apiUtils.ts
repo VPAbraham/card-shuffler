ce1785ef177cce083044ca435932f90f41b0863a
VPAbraham <vpa4565@gmail.com>
2021-07-21 17:09:53 -0600

    Implement apiUtil for requesting a new hand of cards

--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -1,17 +1,35 @@
 export const getNewCardPool = async () => {
-  const handOfCards: string[] = ["init"]
-  const fetchCardPool = async () => {
-    try {
-      const response = await fetch(
-        'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1',
-        { method: 'GET' }
-      );
-      const json = await response.json();
-      console.log(json);
-    }
-    catch (error) {
-      console.log('Request error: ', error);
-    }
+  let newCardPool: object = {};
+  try {
+    const response = await fetch(
+      'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1',
+      { method: 'GET' }
+    );
+    const data: object = await response.json();
+    console.log(data);
+    newCardPool = data;
   }
-  fetchCardPool()
+  catch (error) {
+    console.log('Request error: ', error);
+  }
+  console.log("newcardPool", newCardPool)
+  return newCardPool ? newCardPool : null;
 };
+
+export const getNewCardHand = async () => {
+  let newCardHand: object = {};
+  try {
+    const response = await fetch(
+      'https://deckofcardsapi.com/api/deck/gf7tapc2oshz/draw/?count=5',
+      { method: 'GET' }
+    );
+    const data: object = await response.json();
+    console.log(data);
+    newCardHand = data;
+  }
+  catch (error) {
+    console.log('Request error: ', error);
+  }
+  console.log("newCardHand", newCardHand);
+  return newCardHand ? newCardHand : null;
+}
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,11 +1,21 @@
 import CardContainer from '../components/CardContainer';
-import { useEffect } from 'react';
-import { getNewCardPool } from '../utils/apiUtils';
+import { useEffect, useState } from 'react';
+import { getNewCardPool, getNewCardHand } from '../utils/apiUtils';
 
 const Home = () => {
+  const [numberOfHands, setNumberOfHands] = useState(1);
+  const [cardPool, setCardPool] = useState(null);
+  const [cardHands, setCardHands] = useState([]);
+
   useEffect(() => {
     getNewCardPool();
+    getNewCardHand();
   })
+
+  const retrieveNewCards = async () => {
+    const deckId = await getNewCardPool();
+    console.log("deckID", deckId);
+  }
   return (
     <div>
       <h2>Home Page</h2>
