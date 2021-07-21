69146d4b2e945865eb772665254dbbe6ced61576
VPAbraham <vpa4565@gmail.com>
2021-07-21 13:35:33 -0600

    Install SASS

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  APP
     </div>
   );
 }
