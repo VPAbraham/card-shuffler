1b398316e87f39a058185a9d145081bb5adeb730
VPAbraham <vpa4565@gmail.com>
2021-07-24 13:28:29 -0600

    Update some styling for tthe nav menu, add open and closed hamburger menu states

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from 'react';
+import { ReactElement, useState } from 'react';
 import '../styles/NavBar.scss';
 
 type NavBarProps = {
@@ -8,22 +8,38 @@ type NavBarProps = {
 }
 
 const NavBar = ({ button, dropdown1, dropdown2 }: NavBarProps) => {
+  const [hamburgerIsOpen, setHamburgerIsOpen] = useState(false);
   return (
-    <nav className='navbar nav-bar px-3 py-2'>
+    <nav className='navbar nav-bar px-3 py-2' role='navigation' aria-label='main naviagtion'>
       <div className='navbar-brand'>
-        <h1>CARD SHUFFLER</h1>
+        <a className='navbar-item' >
+          <h1 className='is-white'>Card Shuffler</h1>
+        </a>
+        <a
+          role="button"
+          className="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navBarItems"
+          onClick={() => setHamburgerIsOpen(!hamburgerIsOpen)}
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
-      <div className='navbar-menu'>
-        <div className='navbar-end'>
-          <span className='navbar-item'>
+
+      <div id='navBarItems' className={`navbar-menu ${hamburgerIsOpen && 'is-active'}`}>
+        <div className='navbar-start'>
+          <a className='navbar-item'>
             {dropdown1()}
-          </span>
-          <span className='navbar-item'>
+          </a>
+          <a className='navbar-item'>
             {dropdown2()}
-          </span>
-          <span className='navbar-item'>
+          </a>
+          <a className='navbar-item'>
             {button}
-          </span>
+          </a>
         </div>
       </div>
     </nav>
